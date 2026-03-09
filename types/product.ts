714ec084187c1fc3f2cc3cbd47d4f69824581cb7
Bryan <fnirmal802@gmail.com>
2026-03-09 06:49:29 +0530

    type fix

--- types/product.ts
+++ types/product.ts
@@ -21,30 +21,6 @@ export interface Product {
 
   quantity: number;
   price: number;
+
+  image_url: string;
 }
-
-
-
-
-        
-//           <Link key={p.id} href={`/product/${p.id}`}>
-//             <div className="flex flex-col gap-2 relative">
-//               <Image
-//                 src={"/WR136011U2/WR136011U2-1.webp"}
-//                 width={250}
-//                 height={250}
-//                 alt={p.product_code}
-//                 className="w-full"
-//               />
-//               <p className="absolute bottom-25 right-5 bg-green-900 p-2 rounded-2xl text-white text-xs font-semibold">
-//                 LKR {p.price}
-//               </p>
-//               <div>
-//                 <h3>{p.product_name}</h3>
-//                 <p className="text-xs opacity-70">{p.description}</p>
-//                 <p className="text-[10px] opacity-70">{p.product_code}</p>
-//               </div>
-//             </div>
-//           </Link>
-//         ))}
-//       </div>
